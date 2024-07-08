ad97dbdd6b0eeeba072ec9d8ce1b6eb211780c0e
jero925 <mygameoption@gmail.com>
2024-07-07 21:35:45 -0300

    WIP to PC

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,22 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { spinnerInterceptor } from './core/interceptors/spinner.interceptor';
 import { ɵBrowserAnimationBuilder } from '@angular/animations';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [
+    provideRouter(routes),
     provideHttpClient(withFetch(), withInterceptors([spinnerInterceptor])),
     provideAnimationsAsync(),
-  importProvidersFrom([ɵBrowserAnimationBuilder])]
+    importProvidersFrom([ɵBrowserAnimationBuilder]),
+    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' },
+  ],
 };
--- src/app/books/book-detail/book-detail.component.ts
+++ src/app/books/book-detail/book-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, computed, inject } from '@angular/core';
+import { Component, OnInit, computed, effect, inject, model, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
@@ -8,142 +8,180 @@ import {
   MatDialogTitle,
 } from '@angular/material/dialog';
 import { VolumeInfo } from '../../core/interfaces/books';
-import { MatGridListModule, MatGridTileText } from '@angular/material/grid-list';
+import {
+  MatGridListModule,
+  MatGridTileText,
+} from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { NotionBooksService } from '../../core/services/notion-books.service';
 import { NotionBookItem } from '../../core/interfaces/notion_books/notion-books';
 import { BooksService } from '../../core/services/books.service';
-import { NotionBookOptionsResults, SelectNombre, SelectOption } from '../../core/interfaces/notion_books/select_options';
+import {
+  NotionBookOptionsResults,
+  SelectNombre,
+  SelectOption,
+} from '../../core/interfaces/notion_books/select_options';
 import { AsyncPipe } from '@angular/common';
 import { NewBook } from '../../core/interfaces/notion_books/create_book';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDatepickerInputEvent, MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatNativeDateModule,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-book-detail',
   standalone: true,
+  providers: [provideNativeDateAdapter()],
   imports: [
     MatDialogTitle,
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
     MatButtonModule,
-    MatGridListModule, 
+    MatGridListModule,
     MatGridTileText,
     MatIconModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
     AsyncPipe,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './book-detail.component.html',
-  styleUrl: './book-detail.component.css'
+  styleUrl: './book-detail.component.css',
 })
 export class BookDetailComponent implements OnInit {
-  public existeLibro: boolean = false
+  public existeLibro: boolean = false;
 
   readonly data = inject<VolumeInfo>(MAT_DIALOG_DATA);
   public authors = computed(() => {
-    const { authors } = this.data
+    const { authors } = this.data;
     return Object.values(authors);
-  })
+  });
 
-  public notionBookData?: NotionBookItem
-  public isbn13: string | null
+  public notionBookData?: NotionBookItem;
+  public isbn13: string | null;
 
   //Inicialización de FormControl
-  public scoreControl: FormControl = new FormControl('')
-  public genreControl: FormControl = new FormControl('')
-  public statusControl: FormControl = new FormControl('')
+  public scoreControl: FormControl = new FormControl('');
+  public genreControl: FormControl = new FormControl('');
+  public statusControl: FormControl = new FormControl('');
 
   //Utilizados para el poblado de los combo-box
   public genres: SelectNombre[];
-  public comboBoxValues: NotionBookOptionsResults
+  public comboBoxValues: NotionBookOptionsResults;
 
   //Nombres Combo-box
   public comboPuntaje: string;
   public comboEstado: string;
   public comboGenres: string;
-  
-  // //Valores de los Combo-Box
+
+  // Valores de los Combo-Box
   public scoreValue: string | null;
   public statusValue: string;
   public genreValue: string;
 
+  //Fechas
+  selectedStartDate = signal<string>('')
 
-  constructor(private notionBookService: NotionBooksService, private bookService: BooksService) { 
+  constructor(
+    private notionBookService: NotionBooksService,
+    private bookService: BooksService,
+    private _snackBar: MatSnackBar
+  ) {
     this.scoreControl.valueChanges.subscribe((value) => {
-      this.scoreValue = value
+      this.scoreValue = value;
     });
 
     this.statusControl.valueChanges.subscribe((value) => {
-      this.statusValue = value
+      this.statusValue = value;
     });
 
     this.genreControl.valueChanges.subscribe((value) => {
-      this.genreValue = value
+      this.genreValue = value;
     });
+
+    effect( () => {console.log('Fecha: ' + this.selectedStartDate())})
   }
 
   ngOnInit(): void {
-    this.isbn13 = this.bookService.getISBN_13(this.data)
-    this.notionBookService.getBookByISBN13(this.isbn13).subscribe((res => {
+    this.isbn13 = this.bookService.getISBN_13(this.data);
+    this.notionBookService.getBookByISBN13(this.isbn13).subscribe((res) => {
       if (!res.message) {
         this.existeLibro = true;
-        this.notionBookData = res
+        this.notionBookData = res;
         console.log(this.notionBookData);
-
       }
-      this.getComboBoxValues()
-      this.getGenres()
-    }))
+      this.getComboBoxValues();
+      this.getGenres();
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 3000,
+    });
   }
 
   getComboBoxValues() {
-    this.notionBookService.getSelectOptions().subscribe((res: NotionBookOptionsResults) => {
-      this.comboBoxValues = res
-      if (this.notionBookData) {
-        const { Estado } = this.notionBookData
-        this.comboEstado = Estado.id
-      }
-    })
+    this.notionBookService
+      .getSelectOptions()
+      .subscribe((res: NotionBookOptionsResults) => {
+        this.comboBoxValues = res;
+        if (this.notionBookData) {
+          const { Estado } = this.notionBookData;
+          this.comboEstado = Estado.id;
+        }
+      });
   }
 
   getGenres() {
     this.notionBookService.getGenres().subscribe((res) => {
-      // console.log(res);
 
-      this.genres = res
+      this.genres = res;
       console.log(this.genres);
 
       if (this.notionBookData) {
-        const { Genre } = this.notionBookData
-        this.comboGenres = Genre[0]
+        const { Genre } = this.notionBookData;
+        this.comboGenres = Genre[0];
       }
-    })
+    });
+  }
+
+  changeStartDate(event: MatDatepickerInputEvent<Date>) {
+    this.selectedStartDate.update(events => `${event.value.toJSON()}`);
   }
 
   onClick() {
     const { title, authors, imageLinks, pageCount } = this.data;
     const newBook: NewBook = {
-      "icon": imageLinks.thumbnail,
-      "cover": imageLinks.thumbnail,
-      "parent": "",
-      "name": title,
-      "author": authors,
-      "pages": pageCount,
-      "status": this.statusValue,
-      "isbn_13": this.isbn13,
-      "year": ["f1d456cd-efcb-4ce1-a9cc-2ec1b5b3dc19"],
-      "start_end": "2024-07-01",
-      "score": this.scoreValue,
-      "genre": [this.genreValue]
-    }
+      icon: imageLinks.thumbnail,
+      cover: imageLinks.thumbnail,
+      parent: '',
+      name: title,
+      author: authors,
+      pages: pageCount,
+      status: this.statusValue,
+      isbn_13: this.isbn13,
+      year: ['f1d456cd-efcb-4ce1-a9cc-2ec1b5b3dc19'],
+      start_end: '2024-07-01',
+      score: this.scoreValue,
+      genre: [this.genreValue],
+    };
     // const estadoSelect = this.selectPuntaje.value
     console.log(newBook);
-    
+    this.notionBookService.createBook(newBook).subscribe((res) => {
+      this.existeLibro = true;
+      this.openSnackBar(res.message, 'Ocultar');
+    });
   }
-
 }
